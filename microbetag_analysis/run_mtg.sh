c5570317f7161329dcd95522255f4276de7bbaa4
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2025-10-29 17:18:55 +0200

    minor edits for reunning prev20

--- microbetag_analysis/run_mtg.sh
+++ microbetag_analysis/run_mtg.sh
@@ -1,27 +1,15 @@
+#!/usr/bin/bash
 
-# conda source 
-conda deactivate
+eval "$(conda shell.bash hook)"
 conda activate microbetag
 
-
 types=("treatment" "day")
-# TYPE=$1
-
-# if [[ "$1" != "day" && "$1" != "treatment" ]]; then
-#     echo "Invalid argument: $1"
-#     echo "Please use 'day' or 'treattmentt'."
-#     exit 1
-# fi
-
 ko_merged_gz="mgg_prec/KEGG_annotations/ko_merged.txt.gz"
-
-
-types=("treatment" "day")
 
 
 run_microbetag_for_type() {
     local TYPE=$1
-    local ko_merged_gz="mgg_prec/KEGG_annotations/ko_merged.txt.gz"  # ko_merged_gz="ko_merged.txt.gz"
+    local ko_merged_gz="mgg_prec/KEGG_annotations/ko_merged.txt.gz"
 
     if [ "$TYPE" == "day" ]; then
         files=(config_day_*.yaml)
@@ -43,65 +31,34 @@ run_microbetag_for_type() {
         else
             echo -e "Building microbetag-annotated network for $TYPE $CASE"
 
-            if [ -f "$ko_merged_gz" ]; then
-                echo -e "Decompressing ko_merged.txt file.."
-                gunzip "$ko_merged_gz"
-            fi
+            #if [ -f "$ko_merged_gz" ]; then
+            #    echo -e "Decompressing ko_merged.txt file.."
+            #    gunzip -k "$ko_merged_gz"   # keep gz file to avoid re-downloading
+            #fi
 
             echo -e "Run microbetag"
             microbetag --config "$config_file" > log 2>&1
 
             if compgen -G "mgg_prec/mtag_net_*" > /dev/null; then
                 rm -f mgg_prec/network_output.edgelist
-		if compgen -G "mgg_prec/*.cyjs" > /dev/null; then
-			rm mgg_prec/*.cyjs
-		fi
+                if ls mgg_prec/*.cyjs >/dev/null 2>&1; then
+                    rm mgg_prec/*.cyjs
+                fi
                 mv mgg_prec/mtag_net_* "microbetag_nets/${CASE}.cx2"
                 echo -e "\n----  microbetag for $TYPE $CASE is complete. ---  \n"
             else
                 echo -e "\n!! Something went wrong, no microbetag network was built for $TYPE $CASE\n"
             fi
         fi
+
+        # ensure clean environment between runs
+        rm -rf __pycache__ .mypy_cache 2>/dev/null
+        unset CASE base part config_file
     done
 }
+
 
 
 for type in day treatment; do
     run_microbetag_for_type "$type"
 done
-
-
-
-# for config_file in *.yaml; do
-
-#     base=$(basename "$config_file")
-#     part="${base#*_}"
-#     CASE="${part%%.*}"
-
-#     if [ -f "microbetag_nets/${TYPE}_${CASE}.cx2" ]; then
-#         echo -e "A microbetag annotated network is already built for $TYPE $CASE."
-
-#     else
-#         echo -e "Building microbetag-annotated network for $TYPE $CASE"
-
-#         # microbetag compresses ko_merged.txt always, so we make sure it's decompressed
-#         if [ -f $ko_merged_gz ]; then
-#             echo -e "Decompressing ko_merged.txt file.."
-#             gunzip $ko_merged_gz
-#         fi
-
-#         # # Run microbetag
-#         echo -e "Run microbetag"
-#         microbetag --config "$config_file" > log 2>&1
-
-#         # Remove the FlashWeave network, otherwise microbetag will use the previous one and you'll end up with the same network across all your days/treatments.
-#         if compgen -G "mgg_prec/mtag_net_*" > /dev/null; then
-    
-#             rm mgg_prec/network_output.edgelist
-#             mv mgg_prec/mtag_net_* microbetag_nets/${TYPE}_${CASE}.cx2
-#             echo -e "\n----  microbetag for $TYPE $CASE is complete. ---  \n"
-#         else
-#             echo -e "\n!! Something went wrong, no microbetag network was built for $TYPE $CASE\n"
-#         fi
-#     fi
-# done
